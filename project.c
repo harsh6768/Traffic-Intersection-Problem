2a6ba4cefc3dc2a7923ae6d177e34ee1943abe48
harsh chaurasiya <harshchaurasiya6768@gmail.com>
2018-04-07 21:21:46 +0530

    Update project.c

--- project.c
+++ project.c
@@ -2,66 +2,74 @@
 #include<stdlib.h>
 #include<pthread.h>
 
-pthead_mutex_t l;
+pthread_mutex_t l;
 /*
 enum directions{
                 east=1,west=2,north=3,south=4
 };
 */
+// purpose of the generating the random number we just asssuming the random number as a direction
 static int origin1,destination1;
 static int origin2,destination2;
 
-void *vehicle1(void *dest)
-{ 
-   struct direction *v1; 
-   pthead_mutex_lock(&l);
-   origin1=rand()%4+1;
-   destination1=rand()%4+1;
+void *vehicle1()
+{  
+   pthread_mutex_lock(&l);
+   origin1=rand()%4;
+   destination1=rand()%4;    //generates the random direction which is assuming as a random direction
+
    if(origin1==origin2 ||destination1==destination2)
     {
-     printf("Vehicle2 is entering in critical section");
+     printf("Vehicle2 is entering in critical section\n");
      sleep(1);
     }
     else if(origin1==destination2 ||origin2==destination1)
     {
-      printf("Vehicle 2 is entering in critical section");
+      printf("Vehicle 2 is entering in critical section\n");
       sleep(1);
     }
     else if(origin1==origin2  ||destination1!=destination2)
    {
-     printf("Vehicle 2 is entering in critical secition");
+     printf("Vehicle 2 is entering in critical secition\n");
      sleep(1);
-   } 
+   }
+    else{
+         printf("Vehicle 1 and 2 are colliding ");  
+      } 
    pthread_mutex_unlock(&l);
 
 }
-void *vehicle2(void *dest)
-{ 
-  struct direction *v2;
+
+void *vehicle2()
+{
+ 
   pthread_mutex_lock(&l);
-  origin2=rand()%4+1;
-  destination2=rand()%4+1;
+  origin2=rand()%4;
+  destination2=rand()%4;
   if(origin1==origin2  ||destination1==destination2)
   {
-   printf("Vehicle1 is entering in critical section");
+   printf("Vehicle1 is entering in critical section\n");
    sleep(1);
   }
   else if(origin1==destination2 || origin2==destination1)
   {
-    printf("Vehicle 1 is entering in crical section");
+    printf("Vehicle 1 is entering in crical section\n");
     sleep(1);
   }
   else if(origin1==origin2 ||destination1!=destination2)
   {
-    printf("Vehicle 1 is entering in crical section");
+    printf("Vehicle 1 is entering in crical section\n");
     sleep(1);
   }
+  else{
+    printf("Vehicle 1 and 2 are colliding!!!");
+   }
   pthread_mutex_unlock(&l);
   
 }
 int main()
 {
-  pthread_mutex_init(&l,0);       //we don't share any shared variable with the fork
+  pthread_mutex_init(&l,NULL);       //we don't share any shared variable with the fork
  
   pthread_t p,q;
 
