9a28d3b3b217b3ab48ea37bd73a44c8234b96937
harsh chaurasiya <harshchaurasiya6768@gmail.com>
2018-04-08 19:31:58 +0530

    Update project.c

--- project.c
+++ project.c
@@ -15,9 +15,12 @@ static int origin2,destination2;
 void *vehicle1()
 {  
    pthread_mutex_lock(&l);
-   origin1=rand()%4;
-   destination1=rand()%4;    //generates the random direction which is assuming as a random direction
-
+   origin1=rand()%4+1;
+   destination1=rand()%4+1;    //generates the random direction which is assuming as a random direction
+   
+  printf("Origin of the Vehicle1 is:%d\n",origin1);
+  printf("Destination of the Vehicle1 is:%d\n",destination1);
+  
    if(origin1==origin2 ||destination1==destination2)
     {
      printf("Vehicle2 is entering in critical section\n");
@@ -34,7 +37,7 @@ void *vehicle1()
      sleep(1);
    }
     else{
-         printf("Vehicle 1 and 2 are colliding ");  
+         printf("Vehicle 1 and 2 are colliding\n ");  
       } 
    pthread_mutex_unlock(&l);
 
@@ -42,35 +45,43 @@ void *vehicle1()
 
 void *vehicle2()
 {
- 
-  pthread_mutex_lock(&l);
-  origin2=rand()%4;
-  destination2=rand()%4;
-  if(origin1==origin2  ||destination1==destination2)
-  {
-   printf("Vehicle1 is entering in critical section\n");
-   sleep(1);
-  }
-  else if(origin1==destination2 || origin2==destination1)
-  {
-    printf("Vehicle 1 is entering in crical section\n");
-    sleep(1);
-  }
-  else if(origin1==origin2 ||destination1!=destination2)
-  {
-    printf("Vehicle 1 is entering in crical section\n");
-    sleep(1);
-  }
-  else{
-    printf("Vehicle 1 and 2 are colliding!!!");
+    pthread_mutex_lock(&l);
+    origin2=rand()%4+1;
+    destination2=rand()%4+1;
+    printf("\nOrigin of the Vehicle2 is:%d\n",origin2);
+    printf("Destination of the Vehicle2 is:%d\n",destination2);
+
+    if(origin1==origin2  ||destination1==destination2)
+    {
+       printf("Vehicle1 is entering in critical section\n");
+       sleep(1);
+    }
+    else if(origin1==destination2 || origin2==destination1)
+    {
+      printf("Vehicle 1 is entering in crical section\n");
+      sleep(1);
+    }
+    else if(origin1==origin2 ||destination1!=destination2)
+    {
+      printf("Vehicle 1 is entering in crical section\n");
+      sleep(1);
+    }
+   else{
+    printf("Vehicle 1 and Vehicle2 are colliding!!!");
    }
-  pthread_mutex_unlock(&l);
-  
+
+  pthread_mutex_unlock(&l); 
 }
+
 int main()
 {
-  pthread_mutex_init(&l,NULL);       //we don't share any shared variable with the fork
- 
+  printf("-------------------------------------------------------------\n");
+  printf("-------------------------------------------------------------\n");
+  printf("---------------Traffic Intersection Problem------------------\n");
+  printf("I am taking the Two Cars for doing the demo assumption for avoiding the deadlocks!!!\n");
+  printf("Directions:::\nWest=1\nNorth=2\nEast=3\nSouth=4\n");
+
+  pthread_mutex_init(&l,NULL);       //we don't share any shared variable with the fork 
   pthread_t p,q;
 
   pthread_create(&p,NULL,vehicle1,NULL);
